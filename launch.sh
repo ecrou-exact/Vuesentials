103289a701293f2df18980769561b9c8b57f87f2
ecrou-exact <geffetheo@gmail.com>
2026-03-02 14:05:33 +0100

    chg: [flask] new flask template

--- launch.sh
+++ launch.sh
@@ -5,11 +5,29 @@ function launch {
     python3 app.py
 }
 
+function test {
+    export FLASKENV="testing"
+    pytest
+}
+
+function init_db {
+	python3 app.py -i
+}
+
+function reload_db {
+	python3 app.py -r
+}
 
 
 if [ "$1" ]; then
     case $1 in
-        -l | --launch )             launch;;                               
+        -l | --launch )             launch;
+                                        ;;
+		-i | --init_db )            init_db;
+                                        ;;
+		-r | --reload_db )          reload_db;
+                                        ;;
+        -t | --test )               test;
     esac
     shift
 else
